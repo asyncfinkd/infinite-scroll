e54c706ef867a30b983dce703903fb957d717b97
asyncfinkd <shamgoingzuck@gmail.com>
2021-12-13 20:31:27 +0400

    get item by object

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,18 +3,20 @@ import { Link } from "react-router-dom";
 import { String } from "shared/toString";
 
 export default function Card({ item }) {
+  const { prefix, name, lastName, id, imageUrl, title } = item;
+
   return (
     <>
-      <Link style={{ textDecoration: "none" }} to={String(item.id)}>
+      <Link style={{ textDecoration: "none" }} to={String(id)}>
         <div className="card" key={item}>
           <div>
-            <img src={item.imageUrl} alt="" className="card__image" />
+            <img src={imageUrl} alt="" className="card__image" />
           </div>
           <div className="card__description">
             <div>
-              {item.prefix} {item.name} {item.lastName}
+              {prefix} {name} {lastName}
             </div>
-            <div>{item.title}</div>
+            <div>{title}</div>
           </div>
         </div>
       </Link>
