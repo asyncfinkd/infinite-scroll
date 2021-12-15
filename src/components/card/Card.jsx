54074c897305592494f837edb56d271e392b92ab
asyncfinkd <shamgoingzuck@gmail.com>
2021-12-15 20:30:17 +0400

    add user in context

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import { ApplicationContext } from 'context/ApplicationContext'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { String } from 'shared/toString'
 
 export default function Card({ item }) {
+  const { contextValue } = useContext(ApplicationContext)
+
   const { prefix, name, lastName, id, imageUrl, title } = item
 
   return (
     <>
-      <Link style={{ textDecoration: 'none' }} to={`profile/${String(id)}`}>
+      <Link
+        style={{ textDecoration: 'none' }}
+        to={`profile/${String(id)}`}
+        onClick={() => {
+          contextValue.push(item)
+        }}
+      >
         <div className="card" key={item}>
           <div>
             <img src={imageUrl} alt="" className="card__image" />
